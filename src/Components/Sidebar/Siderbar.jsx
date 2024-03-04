dbac28fcdc6e165bfaf5a329ac315daede373278
Matt Yu <matthewwangyu@gmail.com>
2024-03-04 13:24:52 -0500

    Changes some of the sidebar -- Decided to use MUI React Component library.
    Still not finished - Needs styling

--- src/Components/Sidebar/Siderbar.jsx
+++ src/Components/Sidebar/Siderbar.jsx
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Sidebar.css';
+import { Link } from 'react-router-dom';
 import { FaHome } from "react-icons/fa";
 import { FaMusic } from "react-icons/fa";
 import { FaCompass } from "react-icons/fa";
 import { GiHamburgerMenu } from "react-icons/gi";
-
-
-
-
+import { Drawer, Button } from '@mui/material';
 
 const Sidebar = () => {
-    return (
-        <>
-        <div id='sidebar-burger'>
-            <GiHamburgerMenu />
-        </div>
+    const [open, setOpen] = useState(false);
+
+    const toggleDrawer = (status) => () => {
+        setOpen(status);
+    };
+
+    const DrawerList = (
         <div id='sidebar-container'>
-            <ul>
-                <li><a href='/'><FaHome />      Home</a></li>
-                <li><a href=''><FaMusic />      Library</a></li>
-                <li><a href=''><FaCompass />      Explore</a></li>
-            </ul>
+            <a href="/">
+                <FaHome /> Home
+            </a>
+            <a href="/music">
+                <FaMusic /> Music
+            </a>
+            <a href="/explore">
+                <FaCompass /> Explore
+            </a>
         </div>
+    );
+
+    return(
+        <>
+            <Button onClick={toggleDrawer(true)}><GiHamburgerMenu /></Button>
+            <Drawer open={open} onClose={toggleDrawer(false)}>
+                {DrawerList}
+            </Drawer>
         </>
+        
     );
 }
 
