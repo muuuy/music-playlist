3b0c8f4fa17db849b0357ca173b6af13910bed83
amysev <amys4@umbc.edu>
2024-03-14 15:56:38 -0400

    Updated navbar css and jsx to work with search functionality

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,61 @@
 import React, { useState } from "react";
 import './Navbar.css';
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import { FaHome } from "react-icons/fa";
 import { FaMusic } from "react-icons/fa";
 import { FaCompass } from "react-icons/fa";
 import { FaBars } from "react-icons/fa";
+import axios from "axios";
 
 // function ButtonLink({ to, children }) {
 //       return <Link to={to}><button>{children}</button></Link>
 // }
 
+
+
 const Navbar = () => {
+    const navigate = useNavigate();
+
+    const [searchInput, setSearchInput] = useState("");
+    const [searchType, setSearchType] = useState("song");
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+    
+        console.log('Form submitted')
+      
+        // Update the state only when the form is submitted
+        setSearchInput(e.target.elements.search_box.value);
+        setSearchType(e.target.elements.search_type.value);
+
+        console.log('!!!', searchInput)
+      
+        // Connect to Flask
+        axios.post('http://localhost:5000/user_search', {
+          search_box: searchInput,
+          search_type: searchType
+        })
+        .then(response => {
+          console.log(response.data);
+          // Navigate to specific results page depending on search type
+          if (searchType === 'song') {
+            navigate("/results_by_song", { state: { songResults : response.data }});
+          }
+          else if (searchType === 'artist') {
+            navigate("/results_by_artist", { state: { artistResults: response.data }});
+          }
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+      };
+
+  const handleLogin = () => {
+    navigate('/login');
+  }
+  const handleSignup = () => {
+    navigate('/signup');
+  }
 
     const [isDropdownActive, setDropdownActive] = useState(false);
 
@@ -28,18 +73,19 @@ const Navbar = () => {
                     <li className='nav-item'><NavLink to="/"><FaCompass /> Explore</NavLink></li>
                 </ul>
                 <div className="search">
-                    <form>
-                        <input type="text" placeholder="&#x1F50D; Search" name="search_box"/>
-                        <select name="search_type">
+                    <form onSubmit={handleSearch}>
+                        <input type="text" placeholder="&#x1F50D; Search" name="search_box" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
+                        <select name="search_type" value={searchType} onChange={(e) => setSearchType(e.target.value)}>
                             <option value="song">Song</option>
                             <option value="artist">Artist</option>
                         </select>
+                        <button type="submit">Go</button>
                         {/* <ButtonLink to="/results_by_song">Go</ButtonLink> */}
                     </form>
                 </div>
                 <ul className="sign">
-                    <button id='sign-in__button'><a href="#">Sign In</a></button>
-                    <button><a href="#">Sign Up</a></button>
+                    <button id='sign-in__button'><a href="/login">Sign In</a></button>
+                    <button><a href="/signup">Sign Up</a></button>
                 </ul>
                 <div className="toggle-button" onClick={toggleDropdown}><FaBars /></div>
             </nav>
@@ -47,7 +93,7 @@ const Navbar = () => {
             <nav className={`dropdown-menu ${isDropdownActive ? 'active' : ''}`}>
                 <ul>
                     <li id="nav-item"><a href="/"><FaHome /> Home</a></li>
-                    <li id="nav-item"><a href="#"><FaMusic /> Library</a></li>
+                    <li id="nav-item"><a href="/library"><FaMusic /> Library</a></li>
                     <li id='nav-item'><a href="#"><FaCompass /> Explore</a></li>
                 </ul>
             </nav>
