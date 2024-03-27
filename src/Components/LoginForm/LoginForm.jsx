62eb12096edd430cf13b685cc9d79cdc8b5563f7
Matthew Yu <matthewwangyu@gmail.com>
2024-03-27 15:55:24 -0400

    fixed login form styling

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,41 +1,38 @@
 import React from 'react';
-import './LoginForm.css';
+import styles from './LoginForm.module.css';
 import { NavLink } from 'react-router-dom';
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import Navbar from '../../Components/Navbar/Navbar';
 
 const LoginForm = () => {
     return (
-        <div id='login-container'>
+        <div className='navbar-container'>
             <HelmetProvider>
                 <Helmet>
                     <title>Sign In | MusicPlaylists</title>
                 </Helmet>
             </HelmetProvider>       
-            
-            <div id='logo-container'>
-                <h1>Logo / Image Place Holder</h1>
-            </div>
-            <form id='login-form'>
-                <div id='sliver'></div>
-                <div className='login-text'>
+            <h1>Logo / Image Place Holder</h1>
+            <form className='ui-form'>
+                <div className='sliver'></div>
+                <div className='ui-text'>
                     <h2>Sign in to your account</h2>
                 </div>
                 <label for="email">Email
-                    <input id='email' name="email" type='email' autoComplete='email' required />
+                    <input className='ui-input' name="email" type='email' autoComplete='email' required />
                 </label>
                 <label for="username">Username
-                    <input id='username' name="username" type='username' autoComplete='username' required />
+                    <input className='ui-input' name="username" type='username' autoComplete='username' required />
                 </label>
                 <label for="password">Password
-                    <input id='password' name="password" type='password' required/>
+                    <input className='ui-input' name="password" type='password' required/>
                 </label>
-                <div id='signin'>
-                    <button>Sign In</button>
+                <div className='form-button__container'>
+                    <button className='form-button'>Sign In</button>
                 </div>
-                <div id='bottom'>
-                    {/* <a id='forgot'>Forgot Password?</a> */}
-                    <NavLink to='/forgot/' id='forgot'>Forgot Password?</NavLink>
+                <div id={styles.bottom}>
+                    <NavLink to='/forgot/' id={styles.forgot}>Forgot Password?</NavLink>
+                    <NavLink to='/signup/' id={styles.signup_text}>Sign-up</NavLink>
                 </div>
             </form>
         </div>
