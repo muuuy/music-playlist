3f28ce4f1e8a2c7a42e882dc3585f8aee654f278
Matthew Yu <matthewwangyu@gmail.com>
2024-03-27 15:37:02 -0400

    made forgot form styling correct
    added to app.css

--- src/Components/ForgotForm/ForgotForm.jsx
+++ src/Components/ForgotForm/ForgotForm.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import './ForgotForm.css';
+import styles from './ForgotForm.module.css';
 import { NavLink } from 'react-router-dom';
 import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const ForgotForm = () => {
     return (
-        <div id='forgot-container'>
+        <div className='navbar-container'>
             <HelmetProvider>
                 <Helmet>
                     <title>Forgot Password | MusicPlaylists</title>
                 </Helmet> 
             </HelmetProvider>            
             
-            <h1 id='logo-container'>Logo / Image Place Holder</h1>
-            <form id='forgot-form'>
+            <h1>Logo / Image Place Holder</h1>
+            <form className='ui-form'>
                 <div id='sliver'></div>
-                <h2 className='forgot-text'>Forgot Your Password?</h2>
-                <p className='forgot-instruction'>Please enter the email address associated with your account.</p>
+                <h2 className='ui-text'>Forgot Your Password?</h2>
+                <p className={styles.forgot_instruction}>Please enter the email address associated with your account.</p>
                 <label for="email">Email
-                    <input id='email' name="email" type='email' autoComplete='email' required />
+                    <input className='ui-input' name="email" type='email' autoComplete='email' required />
                 </label>
-                <div id='reset-button'>
-                    <button>Reset Password</button>
+                <div className='form-button__container'>
+                    <button className='form-button'>Reset Password</button>
                 </div>
-                <div id='bottom'>
-                    <NavLink to='/login' id='signin-text'>Log In</NavLink>
-                    <NavLink to='/signup' id='signup-text'>Sign Up</NavLink>
+                <div id={styles.bottom}>
+                    <NavLink to='/login' id={styles.signin_text}>Log In</NavLink>
+                    <NavLink to='/signup' id={styles.signup_text}>Sign Up</NavLink>
                 </div>
             </form>
         </div>
