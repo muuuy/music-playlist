0ecb706e4f753d4845f4bb5d76071d24573b7235
Matthew Yu <myu2@umbc.edu>
2024-03-28 12:42:07 -0400

    added create-playlist button

--- src/Pages/Library/Library.jsx
+++ src/Pages/Library/Library.jsx
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
 import styles from './Library.module.css';
 import { Helmet } from "react-helmet";
-import Navbar from '../../Components/Navbar/Navbar';
+import { NavLink } from 'react-router-dom';
 
 class Library extends Component {
     render () {
         return (
-            <div>
+            <div className={styles.library_container}>
                 <Helmet>
                     <title>Library | MusicPlaylists</title>
                 </Helmet>   
 
-                <div>
-                    <div className={styles.playlist__display}>
-                        <img src='' alt='Playlist Image'></img>
-                        <p className='playlist__title'>Hey</p>
-                        <p className='playlist__num-'>Hey</p>
-                        <p>why?</p>
-                        <p className='playlist__temp'>hello?</p>
-                    </div>
+                <div className={styles.library_header}>
+                    <h1>Library</h1>
+                    <button className={styles.create_playlist}><NavLink to='/create_playlist'>Create Playlist</NavLink></button>
                 </div>
-                
+                <div className='sliver'></div>
             </div>
         )
     }
