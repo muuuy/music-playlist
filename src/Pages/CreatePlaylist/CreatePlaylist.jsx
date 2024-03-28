45af06f68d550aa2f29396cfecafc40fdfb2f207
Matthew Yu <myu2@umbc.edu>
2024-03-28 14:02:44 -0400

    added logic / js

--- src/Pages/CreatePlaylist/CreatePlaylist.jsx
+++ src/Pages/CreatePlaylist/CreatePlaylist.jsx
@@ -1,20 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import Navbar from '../../Components/Navbar/Navbar';
 import styles from './CreatePlaylist.module.css'
 
 const CreatePlaylist = () => {
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleTitle = (e) => {
+        setTitle(e.target.value);
+        console.log(e.target.value);
+    }
+
+    const handleDescription = (e) => {
+        setDescription(e.target.value);
+        console.log(e.target.value);
+    }
+
     return (
         <div className={styles.form_container}>
             <div className={styles.input_container}>
                 <h1>Create Playlist</h1>
                 <form className={styles.input_form}>
-                    <label for="title">Title
-                        <input id={styles.title_input} name='title' type='text' required />
+                    <label for={styles.title_input}>Title
+                        <input id={styles.title_input} name='title' type='text' onChange={handleTitle} value={title} required />
                     </label>
-                    <label for="description">Description
-                        <textarea name="description" id={styles.description} cols="100" rows="5"></textarea>
+                    <label for={styles.description}>Description
+                        <textarea name="description" id={styles.description} onChange={handleDescription} value={description} cols="100" rows="5"></textarea>
                     </label>
                     <button id={styles.create_button} type='submit'>Create Playlist</button>
                 </form>
