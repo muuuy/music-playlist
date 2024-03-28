cadc3a57d402d8358c8962c8485e2b910f5dde46
Matthew Yu <myu2@umbc.edu>
2024-03-28 13:32:11 -0400

    added create-playlist page

--- src/Pages/CreatePlaylist/CreatePlaylist.jsx
+++ src/Pages/CreatePlaylist/CreatePlaylist.jsx
@@ -6,15 +6,20 @@ import styles from './CreatePlaylist.module.css'
 
 const CreatePlaylist = () => {
     return (
-        // <div className={styles.container}>
+        <div className={styles.form_container}>
+            <div className={styles.input_container}>
+                <h1>Create Playlist</h1>
+                <form className={styles.input_form}>
+                    <label for="title">Title
+                        <input id={styles.title_input} name='title' type='text' required />
+                    </label>
+                    <label for="description">Description
+                        <textarea name="description" id={styles.description} cols="100" rows="5"></textarea>
+                    </label>
+                    <button id={styles.create_button} type='submit'>Create Playlist</button>
+                </form>
+            </div>
             
-        //     <div className={styles.formContainer}>
-        //         <p>TEST</p>
-        //     </div>
-        // </div>
-        
-        <div className={styles.formContainer}>
-            <p>test</p>
         </div>
         
     )
