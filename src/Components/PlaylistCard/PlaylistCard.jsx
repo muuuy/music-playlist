4c255472b02dbbea947ab8d2e441ffc9bc7603ae
Matthew Yu <myu2@umbc.edu>
2024-04-30 17:30:44 -0400

    added

--- src/Components/PlaylistCard/PlaylistCard.jsx
+++ src/Components/PlaylistCard/PlaylistCard.jsx
@@ -1,9 +1,10 @@
 import styles from './PlaylistCard.module.css'
 
-const PlaylistCard = (id = null) => {
+const PlaylistCard = (userId = null, id = null) => {
     return (
         <>
         <div className={styles.playlistcard_container}>
+            <p className={styles.close}>DELETE</p>
             <img className={styles.playlistcard_img}></img>
             <h1 className={styles.playlist_name}>Test</h1>
         </div>
